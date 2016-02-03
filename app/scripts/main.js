b7d0347077b9979ae64b0eecedb86caa1acd22a1
Emory Griffith <emorygriffith91@gmail.com>
2016-02-02 21:41:27 -0500

    cleaned up code a bit with comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,55 +1,46 @@
-var app = app || {};
-
-
 Parse.initialize("BVRabUTxJBiBlmjrB7czMMEGAUHpNEWe0xqWojf7", "YVlExM39x22WWZ8s3qsG0tvwMGjxCSEvPm7uFHz1");
 var Patient = Parse.Object.extend("Patient");
 
 
-// var query = new Parse.Query(Patient);
-// query.find({
-//   success: function(results) {
-//     console.log(results);
-//     $('.page').append("hi " );
-//
-//   },
-//
-//   error: function(error) {
-//     alert('nooo')
-//   }
-// });
-
+//--------------VIEWS------------------//
 var PatientList = Backbone.View.extend({
   el: '.page',
   render:function(){
       var that = this;
       var query = new Parse.Query(Patient);
-      console.log(query);
       query.find({
         success: function(patients) {
-          console.log(patients);
-
            var obj = {patients};
            var template = _.template($('#patient-list-template').html());
            var html = template(obj);
            that.$el.html(html);
-
-
         },
-
         error: function(error) {
-          alert('nooo didnt findddd');
+          alert('Could not fetch the JSON from Parse server');
         }
-
       }); //end find
-
   }, //end render
-
   error: function(){
         console.log('There was some error in loading and processing the JSON file');
      }
-
-
-
 });
 
 var patientList = new PatientList();
+
+
+//---------------ROUTES--------------//
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'home'
+  }
+});
+
+var router = new Router();
+router.on('route:home', function(){
+  patientList.render();
+});
+
+
+
+//-------Start History------//
+Backbone.history.start();
